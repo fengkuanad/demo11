fb0dafcdc582335b1f416eeeeec39dcbba93e867
offcn-java <1801438988@qq.com>
2019-08-27 19:43:28 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.bean.User;
+import com.example.demo.dao.UserRepository;
+import com.example.demo.service.UserService;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public List<User> getUserList() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void createUser(User user) {
+       userRepository.save(user);
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return userRepository.getOne(id);
+    }
+
+    @Override
+    public void updateUser(Long id, User user) {
+        user.setId(id);
+        userRepository.saveAndFlush(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
